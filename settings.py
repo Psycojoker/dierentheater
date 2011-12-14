e75e8e6396a7abea9d15e9140bdbb8a09da6dec3
Laurent Peuch <cortex@worlddomination.be>
2011-12-15 00:16:50 +0100

    [enh] add the deputies app in the installed apps

--- settings.py
+++ settings.py
@@ -116,6 +116,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'django_extensions',
+    'deputies',
 )
 
 # A sample logging configuration. The only tangible logging
