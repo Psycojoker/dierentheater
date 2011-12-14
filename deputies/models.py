69ff32cf6c68051b8da1dddb7dc05da803da04a8
Laurent Peuch <cortex@worlddomination.be>
2011-12-15 00:16:20 +0100

    [enh] create a model for the Deputy

--- deputies/models.py
+++ deputies/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from djangotoolbox.fields import ListField
 
-# Create your models here.
+class Deputy(models.Model):
+    full_name = models.CharField(max_length=1337)
+    emails = ListField()
+    party_name = models.CharField(max_length=1337)
+    party_url = models.CharField(max_length=1337)
+    #lachambre_id = models.IntegerField(max_length=1337)
+    #first_name = models.CharField(max_length=1337)
+    #last_name = models.CharField(max_length=1337)
+    #websites = ListField()
+
+    def __unicode__(self):
+        return '%s - %s' % (self.full_name, self.party)
