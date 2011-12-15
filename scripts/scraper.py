5313b412bb85f7c170d8a672c257529c545e7842
Laurent Peuch <cortex@worlddomination.be>
2011-12-15 02:53:31 +0100

    [mod] small refactoring to get the link from an a tag more easily

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -5,6 +5,9 @@ from BeautifulSoup import BeautifulSoup
 
 from deputies.models import Deputy, Party
 
+def href(a):
+    return dict(a.attrs)['href']
+
 def get_or_create(klass, _id=None, **kwargs):
     if _id is None:
         object = klass.objects.filter(**kwargs)
@@ -21,7 +24,7 @@ def read_or_dl(url, name):
         text = open('dump/%s' % name).read()
     else:
         text = urlopen(url).read()
-        open('dump/%s' % name, "w").write(text).close()
+        open('dump/%s' % name, "w").write(text)
     return BeautifulSoup(text)
 
 def clean():
@@ -34,10 +37,10 @@ def deputies_list():
     for dep in soup.findAll('table')[4].findAll('tr'):
         items = dep.findAll('td')
         full_name = re.sub('  +', ' ', items[0].a.text)
-        url = dict(items[0].a.attrs)['href']
+        url = href(items[0].a)
         party = get_or_create(Party, name=items[1].a.text, url=dict(items[1].a.attrs)['href'])
         email = items[2].a.text
-        website = dict(items[3].a.attrs)['href'] if items[3].a else None
+        website = href(items[3].a) if items[3].a else None
         # yes, one deputies key contains a O instead of an 0, I'm not joking
         lachambre_id = re.search('key=([0-9O]+)', url).groups()[0]
         Deputy.objects.create(full_name=full_name,
