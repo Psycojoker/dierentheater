bb2fdc6b824ad9c5e1f46a7e1024f3ae9b696828
Laurent Peuch <cortex@worlddomination.be>
2011-12-16 01:48:46 +0100

    [enh] add a page per deputies

--- deputies/urls.py
+++ deputies/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls.defaults import patterns, url
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from models import Deputy
 
 urlpatterns = patterns('',
     url(r'^deputy/$', ListView.as_view(model=Deputy), name='list'),
+    url(r'^deputy/(?P<pk>[a-z0-9]+)/$', DetailView.as_view(model=Deputy), name='deputy'),
 )
