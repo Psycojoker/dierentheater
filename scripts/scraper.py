eb0b53ec28862b9a00b37b4f673f01aa18d76f37
Laurent Peuch <cortex@worlddomination.be>
2012-02-07 16:08:08 +0100

    [mod] split document_to_dico into mini functions

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -328,39 +328,49 @@ def deputies():
 
 
 def document_to_dico(table):
+    def build_sub_section(i, dico):
+        sub_section = clean_text(i.td.b.text)
+        if dico.get(sub_section):
+            raise Exception("'%s' is already use as a key for '%s'" % (sub_section, dico[sub_section]))
+        dico[sub_section] = AccessControlDict()
+        dico[sub_section]["head"] = i('td')[1]
+        return sub_section
+
+    def build_pdf_sub_section(i, dico, sub_section):
+        key = clean_text(i.td.text)
+        # we can have a list on joined documents
+        if unicode(key) == u'Document(s) joint(s)/lié(s)':
+            if not dico[sub_section].get(key):
+                dico[sub_section][key] = []
+            dico[sub_section][key].append(i('td')[1])
+        elif dico[sub_section].get(key):
+            raise Exception("'%s' is already use as a key in the sub_section '%s' for '%s'" % (key, sub_section, dico[sub_section][key]))
+        else:
+            dico[sub_section][key] = i('td')[1]
+
+    def build_first_level(i, dico):
+        key = clean_text(i.td.text)
+        # we can get severals Moniter erratum
+        if unicode(key) == 'Moniteur erratum':
+            if not dico.get(key):
+                dico[key] = []
+            dico[key].append(i('td')[1])
+        else:
+            if dico.get(key):
+                raise Exception("'%s' is already use as a key for '%s'" % (key, dico[key]))
+            dico[key] = i('td')[1]
+
     dico = AccessControlDict()
     sub_section = None
     for i in table:
         if i == u"\n" or i.td.text in ("&#13;", "&nbsp;", "&#160;"):
             continue
         if i.td.b:
-            sub_section = clean_text(i.td.b.text)
-            if dico.get(sub_section):
-                raise Exception("'%s' is already use as a key for '%s'" % (sub_section, dico[sub_section]))
-            dico[sub_section] = AccessControlDict()
-            dico[sub_section]["head"] = i('td')[1]
+            sub_section = build_sub_section(i, dico)
         elif i.td.img:
-            key = clean_text(i.td.text)
-            # we can have a list on joined documents
-            if unicode(key) == u'Document(s) joint(s)/lié(s)':
-                if not dico[sub_section].get(key):
-                    dico[sub_section][key] = []
-                dico[sub_section][key].append(i('td')[1])
-            elif dico[sub_section].get(key):
-                raise Exception("'%s' is already use as a key in the sub_section '%s' for '%s'" % (key, sub_section, dico[sub_section][key]))
-            else:
-                dico[sub_section][key] = i('td')[1]
+            build_pdf_sub_section(i, dico, sub_section)
         else:
-            key = clean_text(i.td.text)
-            # we can get severals Moniter erratum
-            if unicode(key) == 'Moniteur erratum':
-                if not dico.get(key):
-                    dico[key] = []
-                dico[key].append(i('td')[1])
-            else:
-                if dico.get(key):
-                    raise Exception("'%s' is already use as a key for '%s'" % (key, dico[key]))
-                dico[key] = i('td')[1]
+            build_first_level(i, dico)
     return dico
 
 
