bf30919229a4c6060a3ce56e7fce5e741328f969
Laurent Peuch <cortex@worlddomination.be>
2012-02-23 03:48:20 +0100

    [mod] extract utils kind of function from documents.py in a separate file

--- lachambre_parser/documents.py
+++ lachambre_parser/documents.py
@@ -40,8 +40,10 @@ from utils import read_or_dl,\
                   get_or_create,\
                   clean_text,\
                   lxml_read_or_dl,\
-                  AccessControlDict,\
                   get_text_else_blank
+
+from documents_utils import document_pdf_part_cutter,\
+                            document_to_dico
 
 def documents():
     for document_page in read_or_dl("http://www.lachambre.be/kvvcr/showpage.cfm?section=/flwb&language=fr&rightmenu=right&cfm=ListDocument.cfm", "all documents")('div', **{'class': re.compile("linklist_[01]")}):
@@ -260,66 +262,3 @@ def handle_document(document):
 
     document.save()
     dico.die_if_got_not_accessed_keys()
-
-
-def document_to_dico(table):
-    def build_sub_section(i, dico):
-        sub_section = clean_text(i.td.b.text)
-        if dico.get(sub_section):
-            raise Exception("'%s' is already use as a key for '%s'" % (sub_section, dico[sub_section]))
-        dico[sub_section] = AccessControlDict()
-        dico[sub_section]["head"] = i('td')[1]
-        return sub_section
-
-    def build_pdf_sub_section(i, dico, sub_section):
-        key = clean_text(i.td.text)
-        # we can have a list on joined documents
-        if unicode(key) == u'Document(s) joint(s)/lié(s)':
-            if not dico[sub_section].get(key):
-                dico[sub_section][key] = []
-            dico[sub_section][key].append(i('td')[1])
-        elif dico[sub_section].get(key):
-            raise Exception("'%s' is already use as a key in the sub_section '%s' for '%s'" % (key, sub_section, dico[sub_section][key]))
-        else:
-            dico[sub_section][key] = i('td')[1]
-
-    def build_first_level(i, dico):
-        key = clean_text(i.td.text)
-        # we can get severals Moniter erratum
-        if unicode(key) == 'Moniteur erratum':
-            if not dico.get(key):
-                dico[key] = []
-            dico[key].append(i('td')[1])
-        else:
-            if dico.get(key):
-                raise Exception("'%s' is already use as a key for '%s'" % (key, dico[key]))
-            dico[key] = i('td')[1]
-
-    dico = AccessControlDict()
-    sub_section = None
-    for i in table:
-        if i == u"\n" or i.td.text in ("&#13;", "&nbsp;", "&#160;"):
-            continue
-        if i.td.b:
-            sub_section = build_sub_section(i, dico)
-        elif i.td.img:
-            build_pdf_sub_section(i, dico, sub_section)
-        else:
-            build_first_level(i, dico)
-    return dico
-
-
-def document_pdf_part_cutter(soup):
-    result = []
-    blob = [soup('tr')[0]]
-    for i in soup('tr')[1:]:
-        if not clean_text(i.text):
-            continue
-        if not i.img or not i.img.get("class") or i.img["class"] != "picto":
-            blob.append(i)
-        else:
-            result.append(blob)
-            blob = [i]
-
-    result.append(blob)
-    return result
---
+++ lachambre_parser/documents_utils.py
@@ -0,0 +1,82 @@
+# -*- coding:Utf-8 -*-
+
+#  Dieren Theater - lachambre.be to json sausage machine
+#  Copyright (C) 2012  Laurent Peuch <cortex@worlddomination.be>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as
+#  published by the Free Software Foundation, either version 3 of the
+#  License, or any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from utils import clean_text,\
+                  AccessControlDict
+
+def document_to_dico(table):
+    def build_sub_section(i, dico):
+        sub_section = clean_text(i.td.b.text)
+        if dico.get(sub_section):
+            raise Exception("'%s' is already use as a key for '%s'" % (sub_section, dico[sub_section]))
+        dico[sub_section] = AccessControlDict()
+        dico[sub_section]["head"] = i('td')[1]
+        return sub_section
+
+    def build_pdf_sub_section(i, dico, sub_section):
+        key = clean_text(i.td.text)
+        # we can have a list on joined documents
+        if unicode(key) == u'Document(s) joint(s)/lié(s)':
+            if not dico[sub_section].get(key):
+                dico[sub_section][key] = []
+            dico[sub_section][key].append(i('td')[1])
+        elif dico[sub_section].get(key):
+            raise Exception("'%s' is already use as a key in the sub_section '%s' for '%s'" % (key, sub_section, dico[sub_section][key]))
+        else:
+            dico[sub_section][key] = i('td')[1]
+
+    def build_first_level(i, dico):
+        key = clean_text(i.td.text)
+        # we can get severals Moniter erratum
+        if unicode(key) == 'Moniteur erratum':
+            if not dico.get(key):
+                dico[key] = []
+            dico[key].append(i('td')[1])
+        else:
+            if dico.get(key):
+                raise Exception("'%s' is already use as a key for '%s'" % (key, dico[key]))
+            dico[key] = i('td')[1]
+
+    dico = AccessControlDict()
+    sub_section = None
+    for i in table:
+        if i == u"\n" or i.td.text in ("&#13;", "&nbsp;", "&#160;"):
+            continue
+        if i.td.b:
+            sub_section = build_sub_section(i, dico)
+        elif i.td.img:
+            build_pdf_sub_section(i, dico, sub_section)
+        else:
+            build_first_level(i, dico)
+    return dico
+
+
+def document_pdf_part_cutter(soup):
+    result = []
+    blob = [soup('tr')[0]]
+    for i in soup('tr')[1:]:
+        if not clean_text(i.text):
+            continue
+        if not i.img or not i.img.get("class") or i.img["class"] != "picto":
+            blob.append(i)
+        else:
+            result.append(blob)
+            blob = [i]
+
+    result.append(blob)
+    return result
