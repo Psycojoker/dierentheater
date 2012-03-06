c8b9a254976d8f9063118eeb62504419eb9ce44c
Laurent Peuch <cortex@worlddomination.be>
2012-03-06 02:47:55 +0100

    [mod] remove useless code

--- lachambre/api.py
+++ lachambre/api.py
@@ -1,7 +1,6 @@
 from tastypie_nonrel.resources import MongoResource
 from tastypie.constants import ALL
 from models import Deputy, Document, Commission, WrittenQuestion
-#from json import loads, dumps
 
 class DeputyResource(MongoResource):
     class Meta:
@@ -23,11 +22,6 @@ class CommissionResource(MongoResource):
         filtering = {
             "lachambre_id": ALL,
         }
-
-    def dehydrate_deputies(self, bundle):
-        #return map(lambda x: "/api/v1/deputy/" + x, bundle.data["deputies"])
-        #print loads(bundle.data["deputies"])
-        return bundle.data["deputies"]
 
 class WrittenQuestionResource(MongoResource):
     class Meta:
