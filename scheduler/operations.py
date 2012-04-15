43bf74e843e5df411c691f76012854a415a9c0d0
Laurent Peuch <cortex@worlddomination.be>
2012-04-15 19:46:20 +0200

    [enh] new command to reparse all the deputies

--- scheduler/operations.py
+++ scheduler/operations.py
@@ -1,6 +1,7 @@
-from lachambre_parser import documents
+from lachambre_parser import documents, deputies
 
 operations = {
     "check_for_new_documents": documents.check_for_new_documents,
     "parse_a_document": documents.handle_document,
+    "reparse_all_deputies": deputies.scrape,
 }
