d4ca940ecf5e11c0b16bee067e813430c5289ea7
Laurent Peuch <cortex@worlddomination.be>
2012-04-16 01:02:23 +0200

    [mod] remove useless django apps

--- settings.py
+++ settings.py
@@ -90,11 +90,8 @@ TEMPLATE_DIRS = (
 )
 
 INSTALLED_APPS = (
-    'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
     'django.contrib.sites',
-    'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
     'lachambre',
