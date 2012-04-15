75880410504f7f92bf498c7c105f1507512c2ee7
Laurent Peuch <cortex@worlddomination.be>
2012-04-16 00:07:43 +0200

    [enh] new operation to parse every documents

--- scheduler/operations.py
+++ scheduler/operations.py
@@ -2,6 +2,7 @@ from lachambre_parser import documents, deputies
 
 operations = {
     "check_for_new_documents": documents.check_for_new_documents,
+    "reparse_all_documents": documents.parse_every_documents,
     "parse_a_document": documents.handle_document,
     "reparse_all_deputies": deputies.scrape,
 }
