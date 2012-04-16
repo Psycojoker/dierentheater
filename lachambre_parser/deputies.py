e188a6b90d62ec635c97bbb633a1c6e1ba34bd7c
Laurent Peuch <cortex@worlddomination.be>
2012-04-16 03:35:46 +0200

    [mod] remove duplicated code and be paranoid on Deputy creation

--- lachambre_parser/deputies.py
+++ lachambre_parser/deputies.py
@@ -54,28 +54,6 @@ def deputies_list(reset=False):
         website = items[3].a['href'] if items[3].a else None
         # yes, one deputies key contains a O instead of an 0, I'm not joking
         lachambre_id = re.search('key=([0-9O]+)', url).groups()[0]
-        Deputy.objects.create(full_name=full_name,
-                              party=party,
-                              url=url,
-                              websites=[website] if website else [],
-                              lachambre_id=lachambre_id,
-                              emails=[email])
-        logger.debug('adding new deputy %s %s %s %s %s' % (lachambre_id.encode("Utf-8"), full_name.encode("Utf-8"), party, email.encode("Utf-8"), website.encode("Utf-8") if website else ''))
-
-
-@retry_on_access_error
-def check_for_new_deputies(reset=False):
-    soup = read_or_dl("http://www.lachambre.be/kvvcr/showpage.cfm?section=/depute&language=fr&rightmenu=right_depute&cfm=/site/wwwcfm/depute/cvlist.cfm", "deputies", reset)
-
-    for dep in soup('table')[4]('tr'):
-        items = dep('td')
-        full_name = re.sub('  +', ' ', items[0].a.text).strip()
-        url = items[0].a['href']
-        party = get_or_create(Party, name=items[1].a.text, url=dict(items[1].a.attrs)['href'])
-        email = items[2].a.text
-        website = items[3].a['href'] if items[3].a else None
-        # yes, one deputies key contains a O instead of an 0, I'm not joking
-        lachambre_id = re.search('key=([0-9O]+)', url).groups()[0]
         if not Deputy.objects.filter(lachambre_id=lachambre_id):
             deputy = Deputy.objects.create(full_name=full_name,
                                            party=party,
@@ -86,6 +64,10 @@ def check_for_new_deputies(reset=False):
             logger.debug('adding new deputy %s %s %s %s %s' % (lachambre_id.encode("Utf-8"), full_name.encode("Utf-8"), party, email.encode("Utf-8"), website.encode("Utf-8") if website else ''))
             logger.info("[NEW] deputy: %s" % deputy)
 
+
+@retry_on_access_error
+def check_for_new_deputies(reset=False):
+    deputies_list(reset)
 
 def scrape():
     for index, deputy in enumerate(list(Deputy.objects.all())):
