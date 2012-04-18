b272dc8bd992bc375d0da4d0bcf603f1a6b9b7f4
Laurent Peuch <cortex@worlddomination.be>
2012-04-18 17:43:47 +0200

    [mod] revert handle_document usage of using lachambre_id as an argument

--- lachambre_parser/documents.py
+++ lachambre_parser/documents.py
@@ -64,7 +64,7 @@ def scrape():
 def parse_every_documents():
     # list otherwise mongodb will timeout if we stay in a query mode
     for document in list(Document.objects.filter(done=False)):
-        handle_document(document.lachambre_id)
+        handle_document(document)
 
 
 def get_new_documents():
@@ -90,8 +90,7 @@ def check_for_new_documents():
                 return
 
 
-def handle_document(lachambre_id):
-    document = Document.objects.get(lachambre_id=lachambre_id)
+def handle_document(document):
     soup = read_or_dl(LACHAMBRE_PREFIX + document.url if not document.url.startswith("http") else document.url, "a document %s" % document.lachambre_id)
     document.full_details_url = soup('table')[4].a["href"]
     # fucking stupid hack because BeautifulSoup fails to parse correctly the html
@@ -111,7 +110,7 @@ def handle_document(lachambre_id):
 
     document.done = True
     document.save()
-    logger.info("parsed document [%s] %s" % (lachambre_id, document.title["fr"]))
+    logger.info("parsed document [%s] %s" % (document.lachambre_id, document.title["fr"]))
     dico.die_if_got_not_accessed_keys()
 
 
