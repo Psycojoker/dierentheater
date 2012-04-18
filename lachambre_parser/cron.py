ef03aeb49665c8ee4ceec861fb37c6723b2c2bed
Laurent Peuch <cortex@worlddomination.be>
2012-04-18 18:05:15 +0200

    [fix] cron.py file was missing

---
+++ lachambre_parser/cron.py
@@ -0,0 +1,16 @@
+from scheduler import send
+
+def test():
+    send("pouet")
+
+def check_for_new_documents():
+    send("check_for_new_documents")
+
+def reparse_all_documents():
+    send("parse_every_documents")
+
+def reparse_all_deputies():
+    send("scrape")
+
+def check_for_new_deputies():
+    send("check_for_new_deputies")
