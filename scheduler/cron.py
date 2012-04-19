56f973817a096002b651d2ae4f6748ffea73add8
Laurent Peuch <cortex@worlddomination.be>
2012-04-19 14:14:28 +0200

    [fix] merge lachambre_parser/cron.py with the one of the scheduler since I'm only using this one now

--- scheduler/cron.py
+++ scheduler/cron.py
@@ -1,13 +1,22 @@
 from send import send
+from time import sleep
+
+from lachambre.models import Document
+
+
+def reparse_all_documents():
+    for document in Document.objects.all():
+        send("parse_a_document;%s" % document.lachambre_id)
+        sleep(30)
+
 
 def check_for_new_documents():
     send("check_for_new_documents")
 
-def reparse_all_documents():
-    send("parse_every_documents")
 
 def reparse_all_deputies():
     send("scrape")
+
 
 def check_for_new_deputies():
     send("check_for_new_deputies")
