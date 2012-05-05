d721a109f4232f77015ea5014c00e2c553def8a0
Laurent Peuch <cortex@worlddomination.be>
2012-05-05 19:50:37 +0200

    [enh] allow scheduler to start with a --continue to avoid waping old tasks

--- scheduler/management/commands/run_scheduler.py
+++ scheduler/management/commands/run_scheduler.py
@@ -1,8 +1,17 @@
+from optparse import make_option
 from django.core.management.base import BaseCommand
 from scheduler.scheduler import run_scheduler
 
+
 class Command(BaseCommand):
     help = 'Run the parsing jobs scheduler'
+    option_list = BaseCommand.option_list + (
+        make_option('--continue',
+            action='store_true',
+            dest='continue',
+            default=False,
+            help='Don\'t wipe the task list at startup'),
+        )
 
     def handle(self, *args, **options):
-        run_scheduler()
+        run_scheduler(options["continue"])
