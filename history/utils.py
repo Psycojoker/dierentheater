b4fcd4cbb6d4512e30b862d8d4b626b4d46c0823
Laurent Peuch <cortex@worlddomination.be>
2012-05-09 06:08:23 +0200

    [fix] unicode

--- history/utils.py
+++ history/utils.py
@@ -2,4 +2,4 @@ from django.conf import settings
 
 
 def irc(message):
-    open(settings.PROJECT_PATH + "/log/irc", "a").write(message.strip() + "\n")
+    open(settings.PROJECT_PATH + "/log/irc", "a").write(message.strip().encode("Utf-8") + "\n")
