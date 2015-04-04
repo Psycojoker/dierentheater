269172d202aee33c1540a850887bc3efd372d201
Laurent Peuch <cortex@worlddomination.be>
2015-04-04 07:30:00 +0200

    [fix] port commission parsing

--- lachambre/models.py
+++ lachambre/models.py
@@ -22,6 +22,7 @@ logger = logging.getLogger('')
 
 from json import dumps
 from datetime import datetime
+from bs4 import NavigableString
 from django.db import models
 from djangotoolbox.fields import ListField, EmbeddedModelField, DictField
 
@@ -202,6 +203,49 @@ class Commission(models.Model, Jsonify):
     deputies = ListField(models.ForeignKey(CommissionMembership))
     seats = DictField()
 
+    @classmethod
+    def fetch_list(klass):
+        soup, suppe = read_or_dl_with_nl("http://www.lachambre.be/kvvcr/showpage.cfm?section=/comm/commissions&language=fr&cfm=/site/wwwcfm/comm/LstCom.cfm&rightmenu=right_cricra", "commissions list")
+        _type = ""
+        for i, j in zip(soup("div", id="story")[1], suppe("div", id="story")[1]):
+            if not isinstance(i, NavigableString) and (i.h4 or i.a):
+                if i.h4:
+                    _type = i.h4.text
+                    _type_nl = j.h4.text
+                elif i.a:
+                    commission = get_or_create(Commission, lachambre_id=int(re.search("com=(\d+)", i.a["href"]).groups()[0]))
+                    commission.type["fr"] = _type
+                    commission.type["nl"] = _type_nl
+                    commission.name["fr"] = i.a.text
+                    commission.name["nl"] = j.a.text
+                    commission.url = i.a["href"]
+
+                    commission.save()
+
+        for com in list(Commission.objects.all()):
+            klass.fetch_one(com)
+
+    @classmethod
+    def fetch_one(klass, commission):
+        soup, suppe = read_or_dl_with_nl(LACHAMBRE_PREFIX + commission.url, "commission %s" % commission.lachambre_id)
+        commission.full_name["fr"] = soup.h1.text
+        commission.full_name["nl"] = suppe.h1.text
+        commission.deputies = []
+        seats = {"fr": {}, "nl": {}}
+        for i, j in zip(soup('p')[2:], suppe('p')[2:]):
+            role = i.b.text[:-1]
+            role_nl = j.b.text[:-1]
+            for dep in i('a'):
+                deputy = Deputy.objects.get(lachambre_id=re.search("key=([O0-9]+)", dep["href"]).groups()[0])
+                membership = get_or_create(CommissionMembership, deputy=deputy, commission=commission)
+                membership.role = role
+                membership.save()
+                commission.deputies.append(membership.id)
+            seats["fr"][role] = map(lambda x: (x[0], len(x[1].split(','))), zip(map(lambda x: x.text[:-1], i('b')[1:]), str(i).split("<br />")[1:]))
+            seats["nl"][role_nl] = map(lambda x: (x[0], len(x[1].split(','))), zip(map(lambda x: x.text[:-1], i('b')[1:]), str(i).split("<br />")[1:]))
+
+        commission.seats = seats
+        commission.save()
 
     def get_url(self):
         return LACHAMBRE_PREFIX + self.url if not self.url.startswith("http") else self.url
--- lachambre_parser/commissions.py
+++ lachambre_parser/commissions.py
@@ -16,13 +16,10 @@
 #  You should have received a copy of the GNU Affero General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import re
 import logging
 logger = logging.getLogger('')
 
-from lachambre.models import Commission, CommissionMembership, Deputy
-from utils import read_or_dl_with_nl, get_or_create, LACHAMBRE_PREFIX
-from bs4 import NavigableString
+from lachambre.models import Commission, CommissionMembership
 
 
 def clean_models():
@@ -31,44 +28,4 @@ def clean_models():
 
 
 def scrape():
-    soup, suppe = read_or_dl_with_nl("http://www.lachambre.be/kvvcr/showpage.cfm?section=/comm/commissions&language=fr&cfm=/site/wwwcfm/comm/LstCom.cfm&rightmenu=right_cricra", "commissions list")
-    _type = ""
-    for i, j in zip(soup("div", id="story")[1], suppe("div", id="story")[1]):
-        if not isinstance(i, NavigableString) and (i.h4 or i.a):
-            if i.h4:
-                _type = i.h4.text
-                _type_nl = j.h4.text
-            elif i.a:
-                commission = get_or_create(Commission, lachambre_id=int(re.search("com=(\d+)", i.a["href"]).groups()[0]))
-                commission.type["fr"] = _type
-                commission.type["nl"] = _type_nl
-                commission.name["fr"] = i.a.text
-                commission.name["nl"] = j.a.text
-                commission.url = i.a["href"]
-
-                commission.save()
-
-    for com in list(Commission.objects.all()):
-        handle_commission(com)
-
-
-def handle_commission(commission):
-    soup, suppe = read_or_dl_with_nl(LACHAMBRE_PREFIX + commission.url, "commission %s" % commission.lachambre_id)
-    commission.full_name["fr"] = soup.h1.text
-    commission.full_name["nl"] = suppe.h1.text
-    commission.deputies = []
-    seats = {"fr": {}, "nl": {}}
-    for i, j in zip(soup('p')[2:], suppe('p')[2:]):
-        role = i.b.text[:-1]
-        role_nl = j.b.text[:-1]
-        for dep in i('a'):
-            deputy = Deputy.objects.get(lachambre_id=re.search("key=([O0-9]+)", dep["href"]).groups()[0])
-            membership = get_or_create(CommissionMembership, deputy=deputy, commission=commission)
-            membership.role = role
-            membership.save()
-            commission.deputies.append(membership.id)
-        seats["fr"][role] = map(lambda x: (x[0], len(x[1].split(','))), zip(map(lambda x: x.text[:-1], i('b')[1:]), str(i).split("<br />")[1:]))
-        seats["nl"][role_nl] = map(lambda x: (x[0], len(x[1].split(','))), zip(map(lambda x: x.text[:-1], i('b')[1:]), str(i).split("<br />")[1:]))
-
-    commission.seats = seats
-    commission.save()
+    Commission.fetch_list()
