9341a229defbe28a7e91fe92fc4c4d1412b98f57
Laurent Peuch <cortex@worlddomination.be>
2015-04-04 07:39:55 +0200

    [enh] port annual reports parser

--- lachambre/models.py
+++ lachambre/models.py
@@ -898,3 +898,18 @@ class AnnualReport(models.Model, Jsonify):
     law_and_article = DictField()
     periodicity = models.CharField(max_length=1337)
     pdf_url = models.URLField()
+
+    @classmethod
+    def fetch_list(klass):
+        for a, url in enumerate(('http://www.lachambre.be/kvvcr/showpage.cfm?section=none&language=fr&cfm=/site/wwwcfm/rajv/rajvlist.cfm?lastreports=y',
+                             'http://www.lachambre.be/kvvcr/showpage.cfm?section=none&language=fr&cfm=/site/wwwcfm/rajv/rajvlist.cfm?lastreports=n')):
+            soup, suppe = read_or_dl_with_nl(url, "annual repports %i" % a)
+
+            for i, j in zip(soup.find('div', id="story")('table')[1].tbody('tr', recursive=False)[::5], suppe.find('div', id="story")('table')[1].tbody('tr', recursive=False)[::5]):
+                get_or_create(AnnualReport,
+                              title={"fr": i('td')[2].text, "nl": j('td')[2].text},
+                              date=i('td')[0].text,
+                              law_and_article={"fr": i('td')[4].text, "nl": j('td')[4].text},
+                              periodicity=re.sub("[^0-9]", "", i('td')[5].text),
+                              pdf_url=i('td')[1].a["href"] if i('td')[1].a else "",
+                              )
--- lachambre_parser/reports.py
+++ lachambre_parser/reports.py
@@ -16,11 +16,9 @@
 #  You should have received a copy of the GNU Affero General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import re
 import logging
 logger = logging.getLogger('')
 from lachambre.models import AnnualReport
-from utils import read_or_dl_with_nl, get_or_create
 
 
 def clean_models():
@@ -29,15 +27,4 @@ def clean_models():
 
 
 def scrape():
-    for a, url in enumerate(('http://www.lachambre.be/kvvcr/showpage.cfm?section=none&language=fr&cfm=/site/wwwcfm/rajv/rajvlist.cfm?lastreports=y',
-                         'http://www.lachambre.be/kvvcr/showpage.cfm?section=none&language=fr&cfm=/site/wwwcfm/rajv/rajvlist.cfm?lastreports=n')):
-        soup, suppe = read_or_dl_with_nl(url, "annual repports %i" % a)
-
-        for i, j in zip(soup.find('div', id="story")('table')[1].tbody('tr', recursive=False)[::5], suppe.find('div', id="story")('table')[1].tbody('tr', recursive=False)[::5]):
-            get_or_create(AnnualReport,
-                          title={"fr": i('td')[2].text, "nl": j('td')[2].text},
-                          date=i('td')[0].text,
-                          law_and_article={"fr": i('td')[4].text, "nl": j('td')[4].text},
-                          periodicity=re.sub("[^0-9]", "", i('td')[5].text),
-                          pdf_url=i('td')[1].a["href"] if i('td')[1].a else "",
-                          )
+    AnnualReport.fetch_list()
