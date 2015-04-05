7a1d89adefa3a7ba598bc3bd63d08359415a0fae
Laurent Peuch <cortex@worlddomination.be>
2015-04-05 13:31:29 +0200

    [mod] move http/cache/fs related code from util to scraper

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,3 +1,49 @@
+import logging
+logger = logging.getLogger('')
+
+from os.path import exists
+from urllib import urlopen
+from bs4 import BeautifulSoup
+from lxml import etree
+
+from django.conf import settings
+
+
 class Scraper(object):
     def __init__(self, sync=False):
         self.sync = sync
+
+    def read_or_dl_with_nl(self, url, name, reset=False):
+        soup = self.read_or_dl(url, name, reset=reset)
+        suppe = self.read_or_dl(url.replace("&language=fr", "&language=nl", 1), name + " nl", reset=reset)
+        return soup, suppe
+
+
+    def read_or_dl(self, url, name, reset=False):
+        logger.debug("\033[0;33mparsing %s --- %s\033[0m" % (url, name))
+        if not reset and exists('dump/%s' % name) and settings.CACHE_SCRAPING:
+            text = open('dump/%s' % name).read()
+        else:
+            text = urlopen(url).read()
+            open('dump/%s' % name, "w").write(text)
+        soup = BeautifulSoup(text, "html5lib", from_encoding="latin1")
+        if soup.title.text == "404 Not Found":
+            raise IndexError
+        return soup
+
+
+    def lxml_read_or_dl_with_nl(self, url, name, reset=False):
+        soup = self.lxml_read_or_dl(url, name, reset)
+        suppe = self.lxml_read_or_dl(url.replace("&language=fr", "&language=nl", 1), name + " nl", reset)
+        return soup, suppe
+
+
+    def lxml_read_or_dl(self, url, name, reset=False):
+        logger.debug("LXML parsing %s --- %s" % (url, name))
+        if not reset and exists('dump/%s' % name) and settings.CACHE_SCRAPING:
+            text = open('dump/%s' % name)
+        else:
+            text = urlopen(url)
+            open('dump/%s' % name, "w").write(urlopen(url).read())
+        soup = etree.parse(text, etree.HTMLParser())
+        return soup
--- scraper/utils.py
+++ scraper/utils.py
@@ -20,12 +20,7 @@
 import re
 import logging
 logger = logging.getLogger('')
-from os.path import exists
-from urllib import urlopen, quote
-from bs4 import BeautifulSoup
-from lxml import etree
-
-from django.conf import settings
+from urllib import quote
 
 from history.utils import irc
 
@@ -135,42 +130,6 @@ def clean_text(text):
 def lame_url(url):
     # convert super lame urls of lachambre.be into something uzable
     return quote(url.encode("iso-8859-1"), safe="%/:=&?~#+!$,;'@()*[]")
-
-
-def read_or_dl_with_nl(url, name, reset=False):
-    soup = read_or_dl(url, name, reset=reset)
-    suppe = read_or_dl(url.replace("&language=fr", "&language=nl", 1), name + " nl", reset=reset)
-    return soup, suppe
-
-
-def read_or_dl(url, name, reset=False):
-    logger.debug("\033[0;33mparsing %s --- %s\033[0m" % (url, name))
-    if not reset and exists('dump/%s' % name) and settings.CACHE_SCRAPING:
-        text = open('dump/%s' % name).read()
-    else:
-        text = urlopen(url).read()
-        open('dump/%s' % name, "w").write(text)
-    soup = BeautifulSoup(text, "html5lib", from_encoding="latin1")
-    if soup.title.text == "404 Not Found":
-        raise IndexError
-    return soup
-
-
-def lxml_read_or_dl_with_nl(url, name, reset=False):
-    soup = lxml_read_or_dl(url, name, reset)
-    suppe = lxml_read_or_dl(url.replace("&language=fr", "&language=nl", 1), name + " nl", reset)
-    return soup, suppe
-
-
-def lxml_read_or_dl(url, name, reset=False):
-    logger.debug("LXML parsing %s --- %s" % (url, name))
-    if not reset and exists('dump/%s' % name) and settings.CACHE_SCRAPING:
-        text = open('dump/%s' % name)
-    else:
-        text = urlopen(url)
-        open('dump/%s' % name, "w").write(urlopen(url).read())
-    soup = etree.parse(text, etree.HTMLParser())
-    return soup
 
 
 def table2dic(table):
