8243ea3d0a7e2723471ba68022bca488cd5c4330
Laurent Peuch <cortex@worlddomination.be>
2015-04-06 15:20:52 +0200

    [mod] start to make keywords mendatories to raise bugs

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -9,13 +9,13 @@ from lxml import etree
 from .tasks import async_http
 
 
-def get_with_nl(url, name, cache=False, sync=False):
+def get_with_nl(url, name, cache, sync):
     soup = get(url, name)
     suppe = get(url.replace("&language=fr", "&language=nl", 1), name + " nl")
     return soup, suppe
 
 
-def get(url, name, cache=False, sync=False):
+def get(url, name, cache, sync):
     logger.debug("\033[0;33mparsing %s --- %s\033[0m" % (url, name))
     text = retreive_content(url, name)
     soup = BeautifulSoup(text, "html5lib", from_encoding="latin1")
@@ -27,19 +27,19 @@ def to_soup(html):
     return BeautifulSoup(html, "html5lib", from_encoding="latin1")
 
 
-def lxml_get_with_nl(url, name, cache=False, sync=False):
+def lxml_get_with_nl(url, name, cache, sync):
     soup = lxml_get(url, name)
     suppe = lxml_get(url.replace("&language=fr", "&language=nl", 1), name + " nl")
     return soup, suppe
 
 
-def lxml_get(url, name, cache=False, sync=False):
+def lxml_get(url, name, cache, sync):
     logger.debug("LXML parsing %s --- %s" % (url, name))
     text = retreive_content(url)
     soup = etree.parse(text, etree.HTMLParser())
     return soup
 
-def retreive_content(url, key, cache=False, sync=False):
+def retreive_content(url, key, cache, sync):
     if cache and exists('dump/%s' % key):
         return open('dump/%s' % key, "r").read()
 
